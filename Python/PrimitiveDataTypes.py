e9eaceb04a61a92bae6da1ab44eac601bbca3b93
Jon Reinhold <jon.reinhold15@gmail.com>
2017-11-30 20:46:42 -0600

    Create PrimitiveDataTypes.py

---
+++ Python/PrimitiveDataTypes.py
@@ -0,0 +1,15 @@
+#Primitive types 
+
+booleanX = True 
+floatX = 2.0
+intX = 2
+stringX = "string..."
+
+#array
+arrayX = [True, 2.0, 2, "String"]
+
+print(type(booleanX))
+print(type(floatX))
+print(type(intX))
+print(type(stringX))
+print(type(arrayX))
