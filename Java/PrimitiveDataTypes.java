3ed4fc48740e2d64325d0b3fcd050301e758329f
Jon Reinhold <jon.reinhold15@gmail.com>
2017-12-05 21:03:10 -0600

    Update PrimitiveDataTypes.java

--- Java/PrimitiveDataTypes.java
+++ Java/PrimitiveDataTypes.java
@@ -1,7 +1,8 @@
 public class datatypes {
     public static void main(String[] args) {
         final boolean booleanX = true;
-        public int intX;
+        public int intX = 12;
+        public float floatX = 2.0;
         System.out.println(booleanX);
         System.out.println(intX);
         
